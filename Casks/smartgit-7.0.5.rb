9e9891044a878b375a2fbce74cfb5acb99fa22f4
lykciv <vicky.laurijssen@phpro.be>
2016-11-01 01:28:27 +0100

    Update smartgit-7.0.5.rb

--- Casks/smartgit-7.0.5.rb
+++ Casks/smartgit-7.0.5.rb
@@ -1,6 +1,6 @@
 cask 'smartgit-7.0.5' do
   version '7.0.5'
-  sha256 '165c1d7fbe679997bfa847a93be34bb95a71782e'
+  sha256 '0aae9fb139ca8f2a0d649866e8949b534b08b7dc4b1484b557a5cfe9c3e29413'
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.gsub('.', '_')}.dmg"
   name 'SmartGit'
