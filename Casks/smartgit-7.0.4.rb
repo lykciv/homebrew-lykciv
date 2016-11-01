1aeff4ad279c8d7f69d31c3639675e676c404777
lykciv <vicky.laurijssen@phpro.be>
2016-11-01 01:23:32 +0100

    Create smartgit-7.0.4.rb

---
+++ Casks/smartgit-7.0.4.rb
@@ -0,0 +1,16 @@
+cask 'smartgit-7.0.4' do
+  version '7.0.4'
+  sha256 '56d8b06cd42b796bfabd98486fd97a9cb34996244b5811d97510cf4a5b7b12d7'
+
+  url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.gsub('.', '_')}.dmg"
+  name 'SmartGit'
+  homepage 'https://www.syntevo.com/smartgit/'
+  license :commercial
+
+  app 'SmartGit.app'
+  binary 'SmartGit.app/Contents/MacOS/SmartGit'
+
+  caveats do
+    files_in_usr_local
+  end
+end
